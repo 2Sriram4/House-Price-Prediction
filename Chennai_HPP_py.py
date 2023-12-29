acf7707ecbf38f8b94067a4281e57caab11ea132
Sriram K <113620147+2Sriram4@users.noreply.github.com>
2023-12-29 21:10:37 +0530

    removed matplotlib library

--- Chennai_HPP_py.py
+++ Chennai_HPP_py.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
